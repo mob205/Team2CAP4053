cfdcfb17b6962c7bc056bf9d3abdeb702ed54d11
mob205 <mobslayer205@gmail.com>
2024-10-09 22:31:39 -0400

    Add killing moles by interacting on them with a hammer

---
+++ Assets/EnemyInteractable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInteractable : MonoBehaviour, IInteractable
+{
+    [field: SerializeField] public ToolType RequiredTool { get; private set; }
+    public bool IsInteractable(ToolType tool)
+    {
+        return tool == RequiredTool;
+    }
+
+    public void StartInteract(PlayerInteractor player)
+    {
+        Debug.Log("Interacted");
+        // Some death effect here
+        Destroy(transform.parent.gameObject);
+    }
+
+    public void StopInteract(PlayerInteractor player)
+    {
+
+    }
+}
