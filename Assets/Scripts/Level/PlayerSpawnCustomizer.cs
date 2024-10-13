0ad279aceede69f0dcec8491a2fa0224a36d99de
Jack Carr <ja588361@ucf.edu>
2024-10-13 13:05:33 -0400

    Added 3 new player variants

--- Assets/Scripts/Level/PlayerSpawnCustomizer.cs
+++ Assets/Scripts/Level/PlayerSpawnCustomizer.cs
@@ -10,15 +10,26 @@ public class PlayerSpawnCustomizer : MonoBehaviour
     [SerializeField] private Transform[] m_spawnPoints;
     [SerializeField] private Material[] m_materials;
 
+    // Array to hold different player prefabs
+    [SerializeField] private GameObject[] m_playerPrefabs;
+
     private int m_numPlayers = 0;
 
     public void OnPlayerJoined(PlayerInput input)
     {
-        GameObject player = input.gameObject;
+        // Select the appropriate player prefab for the current player
+        GameObject playerPrefab = m_playerPrefabs[m_numPlayers % m_playerPrefabs.Length];
 
-        player.transform.position = m_spawnPoints[m_numPlayers % m_spawnPoints.Length].position;
-        player.GetComponent<MeshRenderer>().SetMaterials(new List<Material>{ m_materials[m_numPlayers % m_materials.Length]});
+        // Instantiate the player at the spawn point
+        GameObject player = Instantiate(playerPrefab, m_spawnPoints[m_numPlayers % m_spawnPoints.Length].position, Quaternion.identity);
 
+        // Assign the input to the new player
+        player.GetComponent<PlayerInput>().SwitchCurrentControlScheme(input.currentControlScheme);
+
+        // Customize the player with a material
+        player.GetComponent<MeshRenderer>().SetMaterials(new List<Material> { m_materials[m_numPlayers % m_materials.Length] });
+
+        // Update player count
         ++m_numPlayers;
     }
 }
