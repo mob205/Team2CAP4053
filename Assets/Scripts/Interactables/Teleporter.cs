dbe2d702c1fc5b972b556129abc2c571a4b6b9a0
mob205 <mobslayer205@gmail.com>
2024-11-10 15:07:00 -0500

    Remove unused test levels. Add teleport particle effects

--- Assets/Scripts/Interactables/Teleporter.cs
+++ Assets/Scripts/Interactables/Teleporter.cs
@@ -7,14 +7,21 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour
 {
     public Transform TeleportDestination;
-    public LayerMask TeleportLayers;
+    [SerializeField] private LayerMask TeleportLayers;
+    [SerializeField] private ParticleSystem _teleportParticles;
  
     private void OnTriggerEnter(Collider collision)
     {
         var closestPlayer = collision.gameObject;
 
+
         if (Helpers.IsInMask(TeleportLayers, closestPlayer.layer))
         {
+            if(_teleportParticles)
+            {
+                Instantiate(_teleportParticles, closestPlayer.transform.position, Quaternion.identity);
+                Instantiate(_teleportParticles, TeleportDestination.position, Quaternion.identity);
+            }
             closestPlayer.transform.position = TeleportDestination.position;
         }
     }
