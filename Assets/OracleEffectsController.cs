047e3444bf6d83ffd702fb59e39ff42a3e027cd1
mob205 <mobslayer205@gmail.com>
2024-11-16 01:18:35 -0500

    Add oracle spawn and death tweening

--- Assets/OracleEffectsController.cs
+++ Assets/OracleEffectsController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,21 @@ using UnityEngine;
 public class OracleEffectsController : MonoBehaviour
 {
     [SerializeField] private float _fadeInDuration = 1.0f;
-    void Start()
-    {
+    [SerializeField] private float _fadeOutDuration = 1.0f;
 
+    private ParticleSystem _particles;
+    private void Start()
+    {
+        _particles = GetComponentInChildren<ParticleSystem>();
+        transform.localScale = Vector3.zero;
+        transform.DOScale(Vector3.one, _fadeInDuration).SetEase(Ease.OutSine);
+    }
+
+    public void OnKill()
+    {
+        transform.DOKill();
+        transform.SetParent(null, true);
+        _particles.Stop();
+        transform.DOScale(Vector3.zero, _fadeOutDuration).SetEase(Ease.InElastic).OnComplete(() => Destroy(gameObject));
     }
 }
