fe13587c3275363cc5a042dd68fe991eea24fb5d
mob205 <mobslayer205@gmail.com>
2024-11-18 15:53:17 -0500

    Prevent mole from killing players with a hammer

--- Assets/Scripts/Enemy/Deathbox.cs
+++ Assets/Scripts/Enemy/Deathbox.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Deathbox : MonoBehaviour
 {
     [SerializeField] private LayerMask _playerLayer;
+    [SerializeField] private ToolType _ignoreToolType;
 
     private void OnTriggerEnter(Collider other)
     {
+        if(_ignoreToolType != null && other.TryGetComponent(out PlayerInteractor interactor) && interactor.GetHeldToolType() == _ignoreToolType)
+        {
+            return;
+        }
         if(other.TryGetComponent(out PlayerHealth player))
         {
             player.Kill();
