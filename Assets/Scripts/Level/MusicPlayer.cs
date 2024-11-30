e46789fe370978569962cf27fc83904770719b82
mob205 <mobslayer205@gmail.com>
2024-11-30 00:37:42 -0500

    Add game over music and update game over UI

--- Assets/Scripts/Level/MusicPlayer.cs
+++ Assets/Scripts/Level/MusicPlayer.cs
@@ -7,12 +7,14 @@ public class MusicPlayer : MonoBehaviour
     [SerializeField] private AudioSource _pregameMusic;
     [SerializeField] private AudioSource _transition;
     [SerializeField] private AudioSource _gameMusic;
+    [SerializeField] private AudioSource _defeatMusic;
 
     private void Start()
     {
         _pregameMusic.Play();
 
         ReadyUpManager.Instance.OnReadyUp.AddListener(PlayTransition);
+        GameStateManager.Instance.OnGameLose.AddListener(PlayDefeat);
     }
     private void PlayTransition()
     {
@@ -26,5 +28,12 @@ public class MusicPlayer : MonoBehaviour
         {
             _gameMusic.Play();
         }
+    }
+    private void PlayDefeat()
+    {
+        _pregameMusic.Stop();
+        _transition.Stop();
+        _gameMusic.Stop();
+        _defeatMusic.Play();
     }
 }
